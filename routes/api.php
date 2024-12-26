784eb66b4fec6704221da1603c443076a055d57b
nntuhin <tuhin@6amtech.net>
2024-12-26 16:51:34 +0600

    book borrow management

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,13 @@ Route::group(['namespace' => 'api'], function () {
             Route::put('edit/{id}', [BookController::class, 'update']);
             Route::delete('delete/{id}', [BookController::class, 'destroy']);
         });
+        
+        Route::post('/borrow-book', [LibraryController::class, 'borrowBook']);
+        Route::post('/return-book', [LibraryController::class, 'returnBook']);
+        Route::post('/update-last-page', [LibraryController::class, 'updateLastPage']);
+        Route::get('/available-books', [LibraryController::class, 'availableBooks']);
+        Route::get('/current-borrowed-books', [LibraryController::class, 'currentBorrowedBooks']);
+        Route::get('/borrowing-history', [LibraryController::class, 'borrowingHistory']);
 
     });
 });
